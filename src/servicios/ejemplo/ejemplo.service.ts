38b399f939b2a4acaf0b2a994afd16cfdfc05153
aakroos <royal9619@gmail.com>
2024-03-11 22:30:20 -0600

    11/03/24

--- src/servicios/ejemplo/ejemplo.service.ts
+++ src/servicios/ejemplo/ejemplo.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EjemploService {}
+export class EjemploService {
+    getTexto(parametro: string): string {
+        return "El valor del parametro es:" + parametro;
+    }
+}
