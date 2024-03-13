86ed48abbec1760fc16973073da890814d312869
aakroos <royal9619@gmail.com>
2024-03-12 22:46:56 -0600

    12/03/24

--- src/controladores/upload/upload.controller.ts
+++ src/controladores/upload/upload.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';//npm i -D @types/multer
+import { extname } from 'path';
+
+export const storage = diskStorage(
+    {
+        destination: "./assets/uploads",
+        filename: (req, file, callback) => {
+            callback(null, `${Date.now()}${extname(file.originalname)}`);
+        }
+    }
+);
 
 @Controller('upload')
 export class UploadController {
     @Post()
-    @UseInterceptors(FileInterceptor('file'))
+    @UseInterceptors(FileInterceptor('file', {storage}))
     metodoPost(@UploadedFile() file) {
-        return file.originalname;
+        return file;
     }
 }
