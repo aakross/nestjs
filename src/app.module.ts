f4ab23e9e01e079bd743374f014a720848fa7948
sistemas <ti.espiral@gmail.com>
2024-03-13 13:22:59 -0600

    13/03/24

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+
 import { EjemploController } from './controladores/ejemplo/ejemplo.controller';
 import { EjemploService } from './servicios/ejemplo/ejemplo.service';
 import { UploadController } from './controladores/upload/upload.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'assets'),
+    }),
+  ],
   controllers: [AppController, EjemploController, UploadController],
   providers: [AppService, EjemploService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/servicios/ejemplo/ejemplo.service.ts
+++ src/servicios/ejemplo/ejemplo.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class EjemploService {
     getTexto(parametro: string): string { // El parametro se le dice que tipo de dato le vas a pasar si no sabes se pone any y : string es el tipo de dato que vas a retornar
-        return "El valor del parametro es:" + parametro;
+        return "El valor del parametro es:" + parametro+ " | test=" + process.env.TEST;
     }
 }
